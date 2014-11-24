53bc88b7999e129e321f4978eb6a9d23995ed725
Stephan Vock <stephan.vock@gmail.com>
2014-11-24 15:47:33 +0100

    Provider with transactionName and start

---
+++ src/Provider/QafooProfilerServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Easybib\Silex\Provider;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+use Symfony\Component\HttpFoundation\Request;
+use QafooLabs\Profiler;
+
+class QafooProfilerServiceProvider implements ServiceProviderInterface
+{
+    /**
+     * @param Application $app
+     */
+    public function register(Application $app)
+    {
+        if ($app['qafoo.profiler.development']) {
+            Profiler::startDevelopment($app['qafoo.profiler.key']);
+        } else {
+            Profiler::start($app['qafoo.profiler.key']);
+        }
+    }
+
+    /**
+     * @param Application $app
+     */
+    public function boot(Application $app)
+    {
+        $app->before([$this, 'setProfilerTransaction']);
+    }
+
+    /**
+     * @param Request $request
+     */
+    public function setProfilerTransaction(Request $request)
+    {
+        $actionName = $request->get('_route');
+        if (strpos($actionName, '__') === 0) {
+            $actionName = $request->get('_controller');
+        }
+
+        Profiler::setTransactionName(
+            sprintf(
+                '%s %s',
+                $request->getMethod(),
+                $actionName
+            )
+        );
+    }
+}
