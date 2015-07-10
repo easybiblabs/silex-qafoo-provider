efa93da28edceeeb9724a7e97dcb4621c935a134
till <till@php.net>
2015-07-10 13:44:26 +0200

    Fix: proper classname for Qafoolabs\Profiler & CS

--- src/Logger/QafooSQLLogger.php
+++ src/Logger/QafooSQLLogger.php
@@ -31,7 +31,7 @@ class QafooSQLLogger implements SQLLogger
      */
     public function startQuery($sql, array $params = null, array $types = null)
     {
-        if (extension_loaded('tideways') && class_exists('\Tideways\Profiler')) {
+        if (extension_loaded('tideways') && class_exists('Tideways\Profiler')) {
             $this->span = \Tideways\Profiler::createSpan('sql');
             $this->span->startTimer();
             $this->span->annotate(['title' => $sql]);
@@ -39,7 +39,7 @@ class QafooSQLLogger implements SQLLogger
             return;
         }
 
-        if (class_exists('\QafooLabs') and method_exists('\QafooLabs', 'startCustomTimer')) {
+        if (class_exists('QafooLabs\Profiler') and method_exists('QafooLabs\Profiler', 'startCustomTimer')) {
             $this->id = \QafooLabs\Profiler::startCustomTimer('sql', $sql);
         }
     }
